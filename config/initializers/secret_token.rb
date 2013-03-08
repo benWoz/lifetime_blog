4a515c8a4383d26575fb72b685a92ec9a0f81212
Ben Woznicki and Veezus Kreist <b+veezus@benwoz.com>
2013-03-08 11:42:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifetimeBlog::Application.config.secret_token = 'ad526d61fa61a754dd0f465ed7a2fd671dbfff428e66d5df0b2b90794c99c1ef38291108d5658c88ed98639edf838f08a04af36c1b8719ebe4ac5d400a6bebc6'
